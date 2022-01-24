a9dc5901105167d1592dfde96b787682d9ad8fb8
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-01-24 14:24:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+form pyrogram import Client, filters 
 
+
+
+
+
+Alif=Client(
+    "Pyrogram bot",
+    bot_token="bot token",
+    api_id="api id",
+    api_hash="api hash"
+)
+
+
+
+@Alif.on_massage(filters.command("start")) 
+async def start_message
