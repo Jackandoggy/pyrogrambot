323c50a62188e5d6a866dd5389b59170feaec115
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-02-26 16:55:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ async def ok(bot, ok):
 @Alif.on_message(filters.command("video"))
 async def da(bot, da):
     await da.reply_video(
-    Video="https://telegra.ph/file/3c5119738b90107900c08.mp4",
+    video="https://telegra.ph/file/3c5119738b90107900c08.mp4",
     Caption="hi bro",
     Reply_markup=InlineKeyboardMarkup( [[
     InlineKeyboardButton("hi", url="t.me/Alifmuhammed_tg")
