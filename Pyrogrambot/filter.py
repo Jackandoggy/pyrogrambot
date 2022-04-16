579c4381839d0647af1774f71171b42fb19b800f
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-04-16 10:17:51 +0530

    Update filter.py

--- Pyrogrambot/filter.py
+++ Pyrogrambot/filter.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 
 
 
-@Client.on_message(filters.regex("hi"))
+@Client.on_message(filters.regex("hi") | filter.regex("Hi"))                     
 async def regex(bot, msg):
     await msg.reply_text("""
 Hi bro
