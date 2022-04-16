03ef30641d4056989b22e75503a34e53525c2100
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-04-16 10:30:55 +0530

    Update filter.py

--- Pyrogrambot/filter.py
+++ Pyrogrambot/filter.py
@@ -4,7 +4,8 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.regex("hi") | filter.regex("Hi"))                     
 async def regex(bot, msg):
-    await msg.reply_text("Hi bro"),
+    await msg.reply_text(
+        text="hi",
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("𝗱𝗲𝘃", url="https://t.me/Alifmuhammed_tg"),
             InlineKeyboardButton("alert", callback_data="hi"),
