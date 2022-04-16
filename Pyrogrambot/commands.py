804fa0abae3bb4eccddacd9b28ea5541da2a2790
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-04-16 09:32:26 +0530

    Update commands.py

--- Pyrogrambot/commands.py
+++ Pyrogrambot/commands.py
@@ -1,1 +1,26 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import random
 
+
+ALL_PIC = [
+ "https://telegra.ph/file/a34f48501859a206568c1.jpg",
+ "https://telegra.ph/file/d2e8841b6bca68760df39.jpg",
+ "https://telegra.ph/file/0f618d79c4084b48140c2.jpg"
+]
+
+
+@Alif.on_message(filters.private & filters.command("start"))           
+async def start_message(bot, message):
+    await message.reply_photo(
+        photo=random.choice(ALL_PIC),
+        caption=f"hey {message.from_user.mention}do [hi](https://t.me/CinemaChandagroup)",
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("𝗱𝗲𝘃", url="https://t.me/Alifmuhammed_tg"),
+            InlineKeyboardButton("alert", callback_data="hi"),
+            InlineKeyboardButton("Help", callback_data="help")
+            ],[
+            InlineKeyboardButton("NEXT", callback_data="close")
+            ]]
+            )
+         )
