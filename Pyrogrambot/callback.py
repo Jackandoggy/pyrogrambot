99b7e71708265f55d92c18559dc6a2dabfcb81d9
Jackandoggy <98306653+Jackandoggy@users.noreply.github.com>
2022-05-07 12:24:00 +0530

    Update callback.py

--- Pyrogrambot/callback.py
+++ Pyrogrambot/callback.py
@@ -5,20 +5,25 @@ from pyrogram import Client as Alif, filters
 
 @Alif.on_callback_query()
 async def callback (bot, mmt : CallbackQuery):
-    if mmt.data == "mm":  
+    if mmt.data == "hi":  
         await mmt.message.edit(  
             text="hi bro"
         )
      
     elif mmt.data == "help":
-          await mmt.message.edit( 
+
+
+          reply1 = await mmt.message.edit("⬛")
+          reply2 = await reply1.edit("⬛⬛")
+          reply3 = await reply2.edit("⬛⬛⬛")
+          await reply3.edit( 
             text="""Here my help menu
 /id to get id
 /video to get video
 /sticker to get stickers
 """,
             reply_markup=InlineKeyboardMarkup( [[
-               InlineKeyboardButton("😃" , callback_data = "mm")
+               InlineKeyboardButton("hi" , callback_data = "hi")
                ]]
            )
 
@@ -27,5 +32,5 @@ async def callback (bot, mmt : CallbackQuery):
         await mmt.message.delete()
 
 
-    elif mmt.data == "hi":
-        await mmt.answer("𝖸𝗈𝗎 𝖺𝗋𝖾 𝗂𝗇 𝗆𝗒 𝗌𝖾𝖼𝗈𝗇𝖽 𝗉𝖺𝗀𝖾.", show_alert=True)
+    elif mmt.data == "khang":
+        await mmt.answer("Please don't khang the code", show_alert=True)
